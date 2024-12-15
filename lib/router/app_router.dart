843c3c0125dc5dbfa863e736a1c230915ff86f4f
Tomáš Kácha <kachatomas@gmail.com>
2024-12-16 00:12:38 +0100

    feat: add placeholder home screen

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
 import 'package:movie_app/screens/login_screen.dart';
+import 'package:movie_app/screens/home_screen.dart';
 
 part 'app_router.gr.dart';
 
@@ -9,5 +9,6 @@ class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(path: "/login", page: LoginRoute.page, initial: true),
+        AutoRoute(path: "/home", page: HomeRoute.page),
       ];
 }
--- lib/router/app_router.gr.dart
+++ lib/router/app_router.gr.dart
@@ -10,6 +10,25 @@
 part of 'app_router.dart';
 
 /// generated route for
+/// [HomeScreen]
+class HomeRoute extends PageRouteInfo<void> {
+  const HomeRoute({List<PageRouteInfo>? children})
+      : super(
+          HomeRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'HomeRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const HomeScreen();
+    },
+  );
+}
+
+/// generated route for
 /// [LoginScreen]
 class LoginRoute extends PageRouteInfo<void> {
   const LoginRoute({List<PageRouteInfo>? children})
